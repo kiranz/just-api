d89f3c2649357f310cb821657d083be0938797a4
Matthew J. Martin <matthew.mar10@gmail.com>
2020-07-26 16:40:27 +0700

    fix issue 35 honor common_headers if specified from JS

--- test/cli/src/modules/suite_configuration.js
+++ test/cli/src/modules/suite_configuration.js
@@ -1,6 +1,10 @@
 function syncSuiteConfig() {
     this.scheme = 'http';
     this.port = 3027;
+    this.common_headers = [{
+       name: 'content-type',
+       value: 'application/json'
+    }];
 }
 
 module.exports = {
